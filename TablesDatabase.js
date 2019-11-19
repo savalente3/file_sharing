3d16b1a8d33ef3cec871928c10804b0de7333689
Sofia Cirne <valente3@coventry.ac.uk>
2019-11-19 14:32:02 +0000

    upload function constructor and setting koa-send

--- TablesDatabase.js
+++ TablesDatabase.js
@@ -3,6 +3,12 @@
 
 module.exports = class Table {
 
+	static createUserTable() {
+		return `CREATE TABLE IF NOT EXISTS users 
+            (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            username TEXT, email TEXT, pass TEXT);`
+	}
+
 	static createFileTable() {
 		return `CREATE TABLE IF NOT EXISTS files(
         downloadId INTEGER PRIMARY KEY AUTOINCREMENT,
---
+++ routs/filesUpload.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const sqlite = require('sqlite-async')
+const table = require('../TablesDatabase.js')
+
+module.exports = class Upload {
+	constructor(dbName = ':memory:') {
+		return (async() => {
+			try{
+				this.db = await sqlite.open(dbName)
+				await this.db.run(table.createFileTable())
+		  	return this
+			} catch(err) {
+				throw err
+			}
+		})()
+	}
+
+
+}
