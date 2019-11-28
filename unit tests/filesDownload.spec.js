bea9e54d0e0e784eb02759024f772ba1ff803858
Sofia Cirne <valente3@coventry.ac.uk>
2019-11-28 14:56:27 +0000

    projects conflicts

--- unit tests/filesDownload.spec.js
+++ unit tests/filesDownload.spec.js
@@ -9,21 +9,21 @@ describe('download()', () => {
 			const file = await new Download()
 			await file.addDummy(123, 'email@gmail.com', 1, '2019-10-1 13:23:42')
 			await file.download(124)
-			done.failed('test failed')	
+			done.failed('test failed')
 		} catch(err) {
 			expect(err.message).toBe('That file does not exist in the database')
 		} finally {
 			done()
 		}
 	})
-    
+
 	test('File does not exist in the database. (on get name)', async done => {
 		try{
 			expect.assertions(1)
 			const file = await new Download()
 			await file.addDummy(124, 'email@gmail.com', 1, '2019-10-1 13:23:42')
 			await file.getName(125)
-			done.failed('test failed')	
+			done.failed('test failed')
 		} catch(err) {
 			expect(err.message).toBe('That file does not exist in the database')
 		} finally {
