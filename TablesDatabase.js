ee37f1b8d255bfa584b20d1458492c87da1a5a48
Sofia Cirne <valente3@coventry.ac.uk>
2019-11-29 07:05:30 +0000

    bugs: downloadFiles page redirect nav bar blocked to everything except back conflicts between branches fully working upload feature testing to upload

--- TablesDatabase.js
+++ TablesDatabase.js
@@ -14,15 +14,10 @@ module.exports = class Table {
         downloadId INTEGER PRIMARY KEY AUTOINCREMENT,
         filePath TEXT,
         fileName TEXT,
-<<<<<<< HEAD
         uploadDate TEXT,
-=======
-        uploadDate DATETIME,
->>>>>>> ae428917050e40a94412fcafa505e05580bda31d
         senderEmail TEXT,
         receiverEmail TEXT,
         FOREIGN KEY(senderEmail) REFERENCES users(username)
          );`
 	}
-
 }
