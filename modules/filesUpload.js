3b40534d6f8eb9a3f1f4608aa5a06bf48434f31d
handley7 <handley7@uni.coventry.ac.uk>
2019-11-30 15:23:38 +0000

    JSDocs defined for filesUpload.JS

--- modules/filesUpload.js
+++ modules/filesUpload.js
@@ -1,3 +1,11 @@
+/**
+ * This is the filesUpload module
+ * @requires "sqllite-async"
+ * @requires "../TablesDatabase.js"
+ * @requires "fs-extra"
+ * @requires "mime-types"
+ */
+
 'use strict'
 
 const sqlite = require('sqlite-async')
@@ -6,6 +14,10 @@ const fs = require('fs-extra')
 const mime = require('mime-types')
 
 module.exports = class Upload {
+	/**
+	 * Creates an instance of Upload
+	 * @param {*} dbName - the name of the database
+	 */
 	constructor(dbName = ':memory:') {
 		return (async() => {
 			this.db = await sqlite.open(dbName)
@@ -14,6 +26,10 @@ module.exports = class Upload {
 		})()
 	}
 
+	/**
+	 * testUser function
+	 * @async
+	 */
 	async testUser() {
 		await this.db.run(table.createUserTable())
 		const sql = `INSERT INTO users(username, email, pass)
@@ -21,6 +37,11 @@ module.exports = class Upload {
 		await this.db.run(sql)
 	}
 
+	/**
+	 * getSenderEmailWithUsername function, fetches the email address of an inputted user from a database
+	 * @param {*} username 
+	 * @async
+	 */
 	async getSenderEmailWithUsername(username) {
 		const sql1 = `SELECT email FROM users WHERE username = "${username}";`
 		this.senderEmail = await this.db.get(sql1)
@@ -28,6 +49,13 @@ module.exports = class Upload {
 		return this.senderEmail
 	}
 
+	/**
+	 * uploadFiles function copies a file to it's private file path.
+	 * @param {*} path 
+	 * @param {*} mimeType 
+	 * @param {*} fileName 
+	 * @async
+	 */
 	async uploadFiles(path, mimeType, fileName) {
 		try {
 			const extension = mime.extension(mimeType)
@@ -39,9 +67,11 @@ module.exports = class Upload {
 		}
 	}
 
-	// uploads a file with the email of the receiver
-	//gets email of receiver from form input and inserts it into database  with the
-	//username of the sender, file path and file name
+	/**
+	 * sendFileWithReceiverEmail function uploads a file with the email of the receiver and inserts into the database the file name, recipient email and file path.
+	 * @param {*} ReceiverEmail 
+	 * @async
+	 */
 	async sendFileWithReceiverEmail(ReceiverEmail) {
 		try {
 			const sql = `INSERT INTO files(receiverEmail, senderEmail, filePath, fileName)
@@ -56,6 +86,11 @@ module.exports = class Upload {
 	// uploads a file with the username of the receiver
 	// gets email of receiver with the user name and inserts into database the email with the
 	// username of the sender, file path and file name
+	/**
+	 * sendFileWithReceiverUsername function uploads a file with the username of the recipient. Uses username to fetch recipient email. Inserts file name, file path and recipient email into Database
+	 * @param {*} ReceiverUsername 
+	 * @async
+	 */
 	async sendFileWithReceiverUsername(ReceiverUsername) {
 		try {
 			const sql1 = `SELECT email FROM users WHERE username = "${ReceiverUsername}";`
